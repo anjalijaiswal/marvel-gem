018fa938e8f8d5f56b66d4e194c7ebecbc87f434
anjali <anjalijaiswal@users.noreply.github.com>
2018-05-25 18:20:44 +0530

    Add configuration file

---
+++ lib/marvel/configuration.rb
@@ -0,0 +1,10 @@
+module Marvel
+  class Configuration
+    attr_accessor :publicKey, :privateKey
+
+    def initialize
+      @publicKey = "f6f9e528cf58c18c9a282a88c5e7ac08"
+      @privateKey = "84e32e41561d3256f6326f211b36e2a597f28014"
+    end
+  end
+end
